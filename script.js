919714384516bdebf8e5eb221dd0deccfa187ed3
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-01-31 06:48:54 +0100

    Update drawing logic and add animation

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
-let w = 20
+let w = 60;
+let stepsX, stepsY;
+let currentStep = 0;
+let drawnObjects = []; 
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
-    noLoop()
+    createCanvas(windowWidth, windowHeight);
+    stepsX = width / w;
+    stepsY = height / w;
+    frameRate(20);
 
-    const input = document.querySelector('input')
-    input.addEventListener('change', handleSizeChange)
+    for (let i = 0; i < stepsX * stepsY; i++) {
+        drawnObjects.push({ drawn: false });
+    }
 }
 
 function draw() {
-    background(0)
+    let totalSteps = stepsX * stepsY;
+    let easedStep = easeInQuad(min(frameCount, 40), 0, totalSteps, 40);
 
-    let palette = [
-        color(0), color(255), color(7, 33, 95), color(61, 109, 147),
-        color(81, 53, 26), color(115, 192, 171), color(143, 120, 89),
-        color(163, 219, 255), color(175, 17, 34), color(197, 133, 187),
-        color(228, 188, 101), color(236, 76, 91), color(243, 241, 194)
-    ]
+    for (let i = 0; i < easedStep; i++) {
+        if (!drawnObjects[i].drawn) {
+            let palette = [color(0), color(255)];
+            let x = (i % stepsX) * w;
+            let y = Math.floor(i / stepsX) * w;
 
-    drawGrid(palette, w)
-}
+            let squareColor = palette[1];
+            let archColor = palette[0];
 
-function drawGrid(palette, w) {
-    for (let x = 0; x < width; x += w) {
-        for (let y = 0; y < height; y += w) {
-            let shuffledPalette = shuffle([...palette])
-            let squareColor = shuffledPalette[0]
-            let archColor = shuffledPalette[1]
+            noStroke();
+            fill(squareColor);
+            square(x, y, w);
 
-            noStroke()
-            fill(squareColor)
-            square(x, y, w)
+            stroke(archColor);
+            strokeWeight(w / 2);
+            strokeCap(SQUARE);
+            push();
+            translate(x + w / 2, y + w / 2);
+            let offset = int(random(4));
+            rotate((offset * PI) / 2);
+            arc(-w / 2, -w / 2, w * 2 - w / 2, w * 2 - w / 2, 0, PI / 2);
+            pop();
 
-            noFill()
-            stroke(archColor)
-            strokeWeight(w / 2)
-            strokeCap(SQUARE)
-            push()
-            translate(x + w / 2, y + w / 2)
-            let offset = int(random(4))
-            rotate((offset * PI) / 2)
-            arc(-w / 2, -w / 2, w * 2 - w / 2, w * 2 - w / 2, 0, PI / 2)
-            pop()
+            drawnObjects[i].drawn = true;
         }
     }
 }
 
-function handleSizeChange(e) {
-    const size = parseInt(e.target.value)
-    w = size < 10 ? 10 : size
-    redraw()
+function easeInQuad(t, b, c, d) {
+    t /= d;
+    return c*t*t + b;
 }
 
 function shuffle(array) {
-    let currentIndex = array.length, randomIndex
+    let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex--
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-    return array
+    return array;
 }
