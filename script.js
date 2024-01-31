3eb8c595654e691fcab4e6eb632f33c953045e46
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-01-31 08:51:43 +0100

    Refactor animation sequence and add color palette

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let w = 60;
 let stepsX, stepsY;
-let currentStep = 0;
-let drawnObjects = []; 
+let palette;
+let backgroundColor;
+let totalAnimationFrames = 100;
+let drawing = true;
+let isSequenceComplete = false;
+let drawnObjects = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    backgroundColor = color(255, 255, 255);
+    palette = [color(0), color(255)]
     stepsX = width / w;
     stepsY = height / w;
-    frameRate(20);
+    frameRate(60)
 
     for (let i = 0; i < stepsX * stepsY; i++) {
         drawnObjects.push({ drawn: false });
@@ -15,40 +21,58 @@ function setup() {
 }
 
 function draw() {
+    if (isSequenceComplete) return;
+
     let totalSteps = stepsX * stepsY;
-    let easedStep = easeInQuad(min(frameCount, 40), 0, totalSteps, 40);
+    let currentFrame = frameCount % totalAnimationFrames;
+    let easedStep;
 
-    for (let i = 0; i < easedStep; i++) {
-        if (!drawnObjects[i].drawn) {
-            let palette = [color(0), color(255)];
-            let x = (i % stepsX) * w;
-            let y = Math.floor(i / stepsX) * w;
+    if (currentFrame < totalAnimationFrames / 2) {
+        drawing = true;
+        easedStep = easeInQuad(currentFrame, 0, totalSteps, totalAnimationFrames / 2);
+    } else {
+        drawing = false;
+        easedStep = easeInQuad(totalAnimationFrames - currentFrame, 0, totalSteps, totalAnimationFrames / 2);
+    }
 
-            let squareColor = palette[1];
-            let archColor = palette[0];
+    for (let i = 0; i < totalSteps; i++) {
+        let x = (i % stepsX) * w;
+        let y = Math.floor(i / stepsX) * w;
 
-            noStroke();
-            fill(squareColor);
-            square(x, y, w);
-
-            stroke(archColor);
-            strokeWeight(w / 2);
-            strokeCap(SQUARE);
-            push();
-            translate(x + w / 2, y + w / 2);
-            let offset = int(random(4));
-            rotate((offset * PI) / 2);
-            arc(-w / 2, -w / 2, w * 2 - w / 2, w * 2 - w / 2, 0, PI / 2);
-            pop();
-
+        if (drawing && !drawnObjects[i].drawn && i < easedStep) {
+            drawSquareAndArch(x, y, palette[1], palette[0]);
             drawnObjects[i].drawn = true;
+        } else if (!drawing && drawnObjects[i].drawn && i >= easedStep - 1) {
+            drawSquareAndArch(x, y, backgroundColor, backgroundColor);
+            drawnObjects[i].drawn = false;
         }
     }
+
+    if (currentFrame >= totalAnimationFrames - 1) {
+        isSequenceComplete = true;
+        noLoop();
+    }
+}
+
+function drawSquareAndArch(x, y, squareColor, archColor) {
+    noStroke();
+    fill(squareColor);
+    square(x, y, w);
+
+    stroke(archColor);
+    strokeWeight(w / 2);
+    strokeCap(SQUARE);
+    push();
+    translate(x + w / 2, y + w / 2);
+    let offset = int(random(4));
+    rotate((offset * PI) / 2);
+    arc(-w / 2, -w / 2, w * 2 - w / 2, w * 2 - w / 2, 0, PI / 2);
+    pop();
 }
 
 function easeInQuad(t, b, c, d) {
     t /= d;
-    return c*t*t + b;
+    return c * t * t + b;
 }
 
 function shuffle(array) {
